d2b165dc74a5421379ac6bd12ee4e5175bc4d689
khushigour <khushigour0301@gmail.com>
2021-03-28 00:27:05 +0530

    Using middleware functions logic

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req ,res, next)=>{
-   console.log('In the middleware!');
-   next(); //Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended: false})); 
+//It registers themiddleware and will call next so it reaches out middleware before that it will do body parsing but it will not parse all types...
+
+app.use('/add-product', (req, res, next)=> {
+   console.log('Add product');
+   res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>');
+}); 
+
+app.post('/product', (req, res)=>{
+    console.log(req.body);
+    res.redirect('/')
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware');
-    //...
+app.use('/',(req, res, next) => {
+    // console.log('In another middleware');
+    res.send('<h1> Hello from express </h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
