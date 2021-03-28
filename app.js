61de2af4723ae9ce21e8188172a518f1e6be570d
khushigour <khushigour0301@gmail.com>
2021-03-28 13:24:55 +0530

    Separating middleware logic in different file

--- app.js
+++ app.js
@@ -3,22 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false})); 
 //It registers themiddleware and will call next so it reaches out middleware before that it will do body parsing but it will not parse all types...
 
-app.use('/add-product', (req, res, next)=> {
-   console.log('Add product');
-   res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>');
-}); 
-
-app.post('/product', (req, res)=>{
-    console.log(req.body);
-    res.redirect('/')
-});
-
-app.use('/',(req, res, next) => {
-    // console.log('In another middleware');
-    res.send('<h1> Hello from express </h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add-product', (req, res, next)=> {
+    res.send(
+        '<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>'
+        );
+ }); 
+ 
+ router.post('/product', (req, res)=>{
+     res.redirect('/');
+ });
+
+
+ module.exports = router;
