14e61edae79aba50bd79d5e3d30114e25bed81dd
khushigour <khushigour0301@gmail.com>
2021-03-30 11:18:14 +0530

    Serving HTML Files

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,9 @@
 const express = require('express');
-
+const path = require('path');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next)=> {
-    res.send(
-        '<html><body><form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>'
-        );
+    res.sendFile(path.join(__dirname,'..','views','add-product.html'));
  }); 
  
  router.post('/add-product', (req, res)=>{
